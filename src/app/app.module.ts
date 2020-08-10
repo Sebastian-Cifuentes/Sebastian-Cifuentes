f126781c776100007d2ef1f7596cf42f89d3ffb8
Sebastian Cifuentes <sebastianc420@gmail.com>
2020-08-09 20:53:53 -0500

    Firt version of portafolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { APP_ROUTES } from './app.routes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatProgressBarModule,
+    MatCardModule,
+    MatButtonModule,
+    FontAwesomeModule,
+    APP_ROUTES,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
